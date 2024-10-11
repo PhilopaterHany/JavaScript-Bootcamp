a1315c629a6798f5f4993cd452fe6c93850060b4
ahs120 <74141806+ahs120@users.noreply.github.com>
2024-10-11 18:54:49 +0300

    Update Assignment 4.js
    method 2 :D

--- Assignments/Assignments From [ 071 ] To [ 078 ]/Assignment 4.js
+++ Assignments/Assignments From [ 071 ] To [ 078 ]/Assignment 4.js
@@ -7,3 +7,7 @@ let invNums = numsAndStrings
   .map((el) => (-el));
 
 console.log(invNums); // Output => [-1, -10, 10, 20, -5, -3]
+// method 2 
+let mix = [1, 10, -10, -20, 5, "A", 3, "B", "C"];
+onlyNums = mix.filter((ele) => !isNaN(ele)).map((ele) => -ele);
+console.log(onlyNums);
