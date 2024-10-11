7cf2834dba95ec79ff856da75428c578fd18956f
ahs120 <74141806+ahs120@users.noreply.github.com>
2024-10-12 01:43:22 +0300

    Update Assignment 1.js

--- Assignments/Assignments From [ 079 ] To [ 085 ]/Assignment 1.js
+++ Assignments/Assignments From [ 079 ] To [ 085 ]/Assignment 1.js
@@ -12,3 +12,22 @@ console.log(member.name); // Output => Philo
 console.log(member.age); // Output => 16
 console.log(member.country); // Output => Egypt
 console.log(member.fullDetails()); // Output => My Name Is Philo, My Age Is 16, I Live In Egypt
+
+
+// Method 2
+
+
+let member = new Object({});
+member.name = "Elzero";
+member["age"] = 38;
+member.country = "Egypt";
+member.fullDetails = function () {
+  return `My Name Is ${member.name}, My Age Is ${this.age}, I Live In ${this.country}`;
+};
+// Create Your Object Here
+
+console.log(member.name); // Elzero
+console.log(member.age); // 38
+console.log(member.country); // Egypt
+console.log(member.fullDetails());
+// My Name Is Elzero, My Age Is 38, I Live in Egypt
