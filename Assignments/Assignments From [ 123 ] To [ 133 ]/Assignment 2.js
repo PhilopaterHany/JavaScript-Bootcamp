ec4ccbe0ae95762fa39a34dc2567cab29f6e15ac
Philopater Hany <philopaterkiro@gmail.com>
2024-11-12 01:20:07 +0200

    Fixed Assignment Solution

--- Assignments/Assignments From [ 123 ] To [ 133 ]/Assignment 2.js
+++ Assignments/Assignments From [ 123 ] To [ 133 ]/Assignment 2.js
@@ -1,6 +1,6 @@
 let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
 
-console.log(new Set(Array.from(new Set(myFriends)).sort()));
+console.log(Array.from(new Set(myFriends)).sort());
 /*
     Needed Output:
     (4)[("Ahmed", "Mahmoud", "Osama", "Sayed")]
